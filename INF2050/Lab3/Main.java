0143da6dd87bd11ad8266081b6e60a0279499029
Marco <lavallee.marc-antoine.2@courrier.uqam.ca>
2022-01-30 20:52:02 -0500

    Updated FizzBuzz to have 200 iteration. Added gitignore file

--- INF2050/Lab3/Main.java
+++ INF2050/Lab3/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	for (int i = 1; i <= 100; i++ ) {
+	for (int i = 1; i <= 200; i++ ) {
 	        if (i % 5 == 0 && i % 3 == 0) System.out.println("FizzBuzz");
                 else if (i % 5 == 0 ) System.out.println("Buzz");
                 else if (i % 3 == 0 ) System.out.println("Fizz");
